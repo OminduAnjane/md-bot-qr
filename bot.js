dd0b20bce4658c91cd0fc73e6cee5511ab909bf5
Omindu Anjana <93399269+OminduAnjane@users.noreply.github.com>
2022-10-12 21:55:50 +0530

    Update and rename bot.js to lora.js

--- bot.js
+++
@@ -1,7 +0,0 @@
-/* Sri Lanka Qr Genarater 
- Sri Lanka Whatsapp-web-api 
- Sri Lanka Md Bot Bot
- 🇱🇰*/
-
-let {GenarateQrForBotQr} = require('@ominduanjane/queenlora-web-api')
-
---
+++ lora.js
@@ -0,0 +1,8 @@
+/* Sri Lanka Qr Genarater 
+ Sri Lanka Whatsapp-web-api 
+ Sri Lanka Md Bot Bot
+ 🇱🇰*/
+
+let {GenarateQrForQueenLora} = require('@ominduanjane/queenlora-web-api')
+
+/*meka copy karai p## hoo */ 
